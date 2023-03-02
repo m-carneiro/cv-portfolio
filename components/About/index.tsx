15f456010da1d7ededcf6014df39038bd5546a30
m-carneiro <126298357+m-carneiro@users.noreply.github.com>
2023-03-02 15:09:59 -0300

    creation of hero component

---
+++ components/About/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {}
+
+const About = ({}: Props) => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,30 @@
 import Head from 'next/head'
 import {Inter} from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import {NextPage} from "next";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 const inter = Inter({subsets: ['latin']})
 
 const Home: NextPage = () => {
     return (
         <>
-            <div>
+            <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
                 <Head>
                     <title>Matheus Carneiro</title>
-                    <meta name="description" content="Matheusin portfolio show"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
                     <link rel="icon" href="/favicon.ico"/>
                 </Head>
 
-                <Header />
+                <Header/>
 
                 { /* Hero */}
+                <section id={"hero"} className="snap-start">
+                    <Hero/>
+                </section>
 
                 { /* About */}
+                <About />
 
                 { /* Experience */}
 
