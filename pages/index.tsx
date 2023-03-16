0be7dde5354aac6b99f0bbfbcbaa818e01522f47
m-carneiro <126298357+m-carneiro@users.noreply.github.com>
2023-03-16 10:05:11 -0300

    changing portuguese mistakes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import Head from 'next/head'
 import {NextPage} from "next";
 import Header from "@/components/Header";
-// @ts-ignore
 import Hero from "@/components/Hero";
-// @ts-ignore
 import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import React from "react";
+import ContactMe from "@/components/ContactMe";
 
 const Home: NextPage = () => {
     return (
         <>
-            <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+            <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#99FF33]/80">
                 <Head>
                     <title>Matheus Carneiro</title>
                     <link rel="icon" href="/favicon.ico"/>
@@ -43,6 +43,9 @@ const Home: NextPage = () => {
                 </section>
 
                 { /* Contact Me */}
+                <section id={"contact-me"} className={"snap-center"}>
+                    <ContactMe />
+                </section>
 
 
             </div>
