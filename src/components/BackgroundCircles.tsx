d8886ac4967217dfc5810ffbd08767212c2ffba1
m-carneiro <m.carneiro.tech@gmail.com>
2024-02-09 16:39:24 -0300

    fix

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -2,25 +2,25 @@ import { motion } from "framer-motion";
 
 export default function BackgroundCircles() {
 
-    return (
+  return (
     <motion.div
-        initial={{
-            opacity: 0,
-        }}
-        animate={{
-            scale: [1,2,2,2,1],
-            opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
-        }}
-        transition={{
-            duration: 1.5
-        }}
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        scale: [1,2,2,2,1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+      }}
+      transition={{
+        duration: 1.5
+      }}
         
-        className={"relative flex items-center justify-center mt-[120px]"}
-        >
-            <div className="absolute border border-[#99FF33] h-[200px] mt-52 rounded-full w-[200px] animate-ping" />
-            <div className="absolute border border-[#333333] h-[300px] mt-52 rounded-full w-[300px]" />
-            <div className="absolute border border-[#333333] h-[500px] mt-52 rounded-full w-[500px]"/>
-            <div className="absolute border border-[#99FF33] h-[650px] mt-52 rounded-full w-[650px] animate-pulse opacity-20"/>
+      className={"relative flex items-center justify-center mt-[120px]"}
+    >
+      <div className="absolute border border-[#99FF33] h-[200px] mt-52 rounded-full w-[200px] animate-ping" />
+      <div className="absolute border border-[#333333] h-[300px] mt-52 rounded-full w-[300px]" />
+      <div className="absolute border border-[#333333] h-[500px] mt-52 rounded-full w-[500px]"/>
+      <div className="absolute border border-[#99FF33] h-[650px] mt-52 rounded-full w-[650px] animate-pulse opacity-20"/>
     </motion.div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,45 @@
-'use client'
+'use client';
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { SocialIcon } from "react-social-icons";
 
 
 export default function Header() {
-    return (
-      <>
-        <header className={"sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center"}>
+  return (
+    <>
+      <header className={"sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center"}>
+        <motion.div
+          initial={{
+            x: -500,
+            opacity: 0,
+            scale: 0.5
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1
+          }}
+          transition={{
+            duration: 1.5
+          }}
+          className={"flex flex-row items-center"}>
+  
+          <SocialIcon url={"https://www.linkedin.com/in/mtscarneiro23/"}
+            target={"_blank"} fgColor={"gray"}
+            bgColor={"transparent"}/>
+          <SocialIcon url={"https://github.com/m-carneiro"}
+            target="_blank" fgColor={"gray"} 
+            bgColor={"transparent"}/>
+          <SocialIcon url={"https://www.instagram.com/entwicklersec/"}
+            target={"_blank"} fgColor={"gray"}
+            bgColor={"transparent"}/>
+  
+        </motion.div>
+  
+        <Link href="#contact-me">
           <motion.div
             initial={{
-              x: -500,
+              x: 500,
               opacity: 0,
               scale: 0.5
             }}
@@ -20,53 +49,24 @@ export default function Header() {
               scale: 1
             }}
             transition={{
-              duration: 1.5
+              duration: 1
             }}
-            className={"flex flex-row items-center"}>
   
-            <SocialIcon url={"https://www.linkedin.com/in/mtscarneiro23/"}
-              target={"_blank"} fgColor={"gray"}
-              bgColor={"transparent"}/>
-            <SocialIcon url={"https://github.com/m-carneiro"}
-              target="_blank" fgColor={"gray"} 
-              bgColor={"transparent"}/>
-            <SocialIcon url={"https://www.instagram.com/entwicklersec/"}
-              target={"_blank"} fgColor={"gray"}
-              bgColor={"transparent"}/>
+            className={"flex flex-row items-center text-gray-300 cursor-pointer"}>
   
-          </motion.div>
-  
-          <Link href="#contact-me">
-            <motion.div
-              initial={{
-                x: 500,
-                opacity: 0,
-                scale: 0.5
-              }}
-              animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1
-              }}
-              transition={{
-                duration: 1
-              }}
-  
-              className={"flex flex-row items-center text-gray-300 cursor-pointer"}>
-  
-              <div>
-                <SocialIcon
-                  className={"cursor-pointer"}
-                  network={"email"}
-                  fgColor={"gray"}
-                  bgColor={"transparent"}/>
-                <p className={"uppercase hidden md:inline-flex text-sm text-gray-400"}>
+            <div>
+              <SocialIcon
+                className={"cursor-pointer"}
+                network={"email"}
+                fgColor={"gray"}
+                bgColor={"transparent"}/>
+              <p className={"uppercase hidden md:inline-flex text-sm text-gray-400"}>
                                   Get In Touch
-                </p>
-              </div>
-            </motion.div>
-          </Link>
-        </header>
-      </>
-    );
-  }
+              </p>
+            </div>
+          </motion.div>
+        </Link>
+      </header>
+    </>
+  );
+}
